16c82dac27102bca6bcf5bb1c1bb27874f9bf094
JorgeAngst <jorge.rodriguez.web@gmail.com>
2020-05-04 12:20:54 +0200

    iteration 1 done

---
+++ js/memory.js
@@ -0,0 +1,9 @@
+class MemoryGame {
+  constructor(cards){
+    this.cards = cards;
+    // add the rest of the class properties here
+  }
+  shuffleCards() {}
+  checkIfPair(card1, card2) {}
+  isFinished() {}
+}
